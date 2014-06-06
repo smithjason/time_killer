56cdc6c8f6a6c44a49f6ec427fad5023279d3c78
Apprentice <apprentice@dbc26.local>
2014-06-06 11:08:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimeKiller::Application.config.secret_key_base = 'e72136c1f7d9ab185ee6b33ccbc4a1707debebb4d6545b28b3904ccb15901d897e2e352b778b697074afeb86d6e4ec2a816126ae43d37f4af4e4591b35e860e6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TimeKiller::Application.config.session_store :cookie_store, key: '_time_killer_session'
