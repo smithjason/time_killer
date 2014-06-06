9b0db0df618dfd3619c83615a4451fc8e8893044
Apprentice <apprentice@dbc26.local>
2014-06-06 14:55:08 -0700

    A user can sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,9 @@
 class SessionsController < ApplicationController
 
   def new
-    @user=User.new
   end
 
   def create
-
+    user= User.authenticate(params[:email], params[:password])
   end
 end
