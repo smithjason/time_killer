e55ac4afd65bbe5fcced9c5325a6c6a524f90663
Apprentice <apprentice@dbc26.local>
2014-06-06 15:45:49 -0700

    made some rspec'izzle tests for User to test for valid/invalid creation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a name, email, and password" do
+    user=User.new(name: "Tommy", email: "Kitten@kitten.com", password: "ilovecats")
+    expect(user).to be_valid
+  end
+  it "is invalid with no information" do
+    expect(User.create(name: nil, email: nil, password: nil)).to have(1).errors_on(:email)
+  end
+
+  it "is invalid when supplying a non-unique email" do
+  end
 end
