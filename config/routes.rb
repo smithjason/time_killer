4081f66af89f77b1fe541a9d911c80d506ef3e10
Geoff Pavey & Ziad Abdo <pair+gpavey.souldjinn@devbootcamp.com>
2014-06-06 15:55:47 -0700

    can make posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,11 @@ TimeKiller::Application.routes.draw do
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
 
-  root to: 'users#new'
+  root to: 'posts#index'
 
-  get "posts", to: 'posts#index'
-  get "posts/:id", to: 'posts#show'
 
-  resources :users do
-    resources :posts
-  end
+  resources :posts, only: [:index, :new, :create]
+  resources :users
   resources :sessions
 
 end
