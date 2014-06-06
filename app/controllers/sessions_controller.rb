42cd544b6ab9302561632da80b6d84ae95557833
Apprentice <apprentice@dbc26.local>
2014-06-06 15:00:08 -0700

    User can successfully log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,5 +5,11 @@ class SessionsController < ApplicationController
 
   def create
     user= User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      redirect_to log_in_path
+    end
   end
 end
