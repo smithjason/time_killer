91068a007cf46cda88be02f5c6537df8291b0669
Geoff Pavey & Ziad Abdo <pair+gpavey.souldjinn@devbootcamp.com>
2014-06-06 18:35:13 -0700

    init

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ TimeKiller::Application.routes.draw do
   root to: 'posts#index'
 
 
-  resources :posts, only: [:index, :new, :create]
+  resources :posts, only: [:new, :create, :show]
   resources :users
   resources :sessions
 
