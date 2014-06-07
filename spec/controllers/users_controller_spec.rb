960e2450e327fd6ebc59ae7331729c5d50405636
Thomas  Carpenito <thomascarpenito@Tommy.local>
2014-06-07 14:14:34 -0700

    began some testing, unable to mock or stub

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe UsersController do
+	describe "Users #new" do
+		it 'renders the #new template' do 
+			get :new
+			response.should render_template (:new)
+		end
+	end
 
+	describe "Users create" do 
+		# it "creates a new contact" do 
+		# 	new_user = User.create(name: "tommy", email: "t@t.com", password: "t")
+		# 	expect{
+		# 		post :create
+		# 	}.to change(User.count).by(1)
+		# end
+
+
+
+		# it "redirects to signup path on success" do
+		# 	@user= User.create(name: "tommy", email: "t@t.com", password: "t")
+		# 	post :create, user: @user
+		# 	response.should redirect_to(root_url)
+		# end
+	it "redirects to root url on uncucess" do
+	end
+	end
 end
+
