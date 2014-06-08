a4831729940042d2b80e725586c7c8c4f53e5abe
Ziad Abdo <apprentice@dbc05.local>
2014-06-08 16:13:28 -0700

    #I dont know what the f this commit is for, I had some random     deletion issues. I think its fine and fixed.

--- db/migrate/20140608214047_create_votes.rb
+++ db/migrate/20140608214047_create_votes.rb
@@ -1,12 +1,11 @@
 class CreateVotes < ActiveRecord::Migration
   def change
-    create_table :votes do |t|
-      t.belongs_to :user
-      t.integer :up_vote
-      t.integer :down_vote
-      t.references :votable, polymorphic: true
-
-    end
-     add_index :votes, [:votable_id, :votable_type]
+   create_table :votes do |t|
+     t.belongs_to :user
+     t.integer :up_vote
+     t.integer :down_vote
+     t.references :votable, polymorphic: true
+   end
+    add_index :votes, [:votable_id, :votable_type]
   end
 end
