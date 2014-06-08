247d1e8dc2194048800b1defe5f39bc0256660b7
Ziad Abdo <apprentice@dbc05.local>
2014-06-08 15:10:18 -0700

    #Generated migration for votes including polymorphic     association setup.   #Generated the vote model with some association rules.     I think it should be setup well enough to work properly   #Need to generate associations within other models to       complete the associations.

---
+++ db/migrate/20140608214047_create_votes.rb
@@ -0,0 +1,12 @@
+class CreateVotes < ActiveRecord::Migration
+  def change
+    create_table :votes do |t|
+      t.belongs_to :user
+      t.integer :up_vote
+      t.integer :down_vote
+      t.references :votable, polymorphic: true
+
+    end
+     add_index :votes, [:votable_id, :votable_type]
+  end
+end
