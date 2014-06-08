e90a249c57872e588a3c40f3648d0af343a1b0a3
Tommy <null>
2014-06-08 14:53:58 -0700

    Styled loosely with comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,18 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to root_path
+    else
+      redirect_to new_post_comment_path
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
   end
 
 end
